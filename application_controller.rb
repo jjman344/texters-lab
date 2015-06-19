28b5849807d0ec54921cb5e2ebe559b1c229f8d5
Jonathan Shulman <shulmanjj@gmail.com>
2015-06-19 15:13:48 +0000

    initial styling done

--- application_controller.rb
+++ application_controller.rb
@@ -9,17 +9,20 @@ class MyApp < Sinatra::Base
 
 
   get '/' do
-    erb :index
+    erb :homep
   end
   
-  
   post '/' do
+    erb :index
+  end  
+  
+  post '/index' do
     @recipient = params[:recipient]
     @body = params[:choice]
     if @body == ""
       @body = params[:body]
     elsif @body == "R"
-      random = ["What do you call a fake noodle? An impasta!",  "Meet me at the playground, I got the weed", "I have to tell you something really important", "OMG I cant believe that happened to sam"]
+      random = ["What do you call a fake noodle? An impasta!",  "Meet me at the playground, I got the weed", "I have to tell you something really important", " HAHAhaha OMG I cant believe that happened to sam"]
       @body = random[rand(random.length)]
     end
     send_sms(@recipient, @body)
